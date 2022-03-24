2a7cb820800e76a57ea0d56039cf3668dc6c078c
CarStoreFlutterFalcones <sundos.tamimi20@gmail.com>
2022-03-24 12:43:51 +0300

    baraa

--- app/src/main/java/com/example/adminvansales/Report/CustomerLogReport.java
+++ app/src/main/java/com/example/adminvansales/Report/CustomerLogReport.java
@@ -1,10 +1,20 @@
 package com.example.adminvansales.Report;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,6 +23,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,9 +33,14 @@ import com.example.adminvansales.DataBaseHandler;
 import com.example.adminvansales.ExportToExcel;
 import com.example.adminvansales.GlobelFunction;
 import com.example.adminvansales.ImportData;
+import com.example.adminvansales.LocationService;
+import com.example.adminvansales.MainActivity;
+import com.example.adminvansales.PlanSalesMan;
 import com.example.adminvansales.model.CustomerLogReportModel;
 import com.example.adminvansales.PdfConverter;
 import com.example.adminvansales.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -35,8 +51,8 @@ import static com.example.adminvansales.GlobelFunction.salesManNameList;
 
 public class CustomerLogReport extends AppCompatActivity {
 
-    TextView fromDate,toDate;
-    ImageButton excelConvert,pdfConvert,share, backBtn;
+    TextView fromDate, toDate;
+    ImageButton excelConvert, pdfConvert, share, backBtn;
     GlobelFunction globelFunction;
     String toDay;
     CustomerLogReportAdapter customerLogReportAdapter;
@@ -45,52 +61,56 @@ public class CustomerLogReport extends AppCompatActivity {
     ImportData importData;
     Button previewButton;
     Spinner salesNameSpinner;
-    ArrayAdapter<String>salesNameSpinnerAdapter;
+    ArrayAdapter<String> salesNameSpinnerAdapter;
     com.example.adminvansales.model.SettingModel settingModel;
     DataBaseHandler databaseHandler;
+    private BottomNavigationView bottom_navigation;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer_log_report);
-        settingModel=new com.example.adminvansales.model.SettingModel ();
-        databaseHandler=new DataBaseHandler(CustomerLogReport.this);
+        settingModel = new com.example.adminvansales.model.SettingModel();
+        databaseHandler = new DataBaseHandler(CustomerLogReport.this);
 
         initial();
     }
+
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     private void initial() {
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
         builder.detectFileUriExposure();
 
-        fromDate=findViewById(R.id.from_date_r);
-        toDate=findViewById(R.id.to_date_r);
-        listCustomerLogReport=findViewById(R.id.listCustomerLogReport);
-        previewButton=findViewById(R.id.previewButton);
-        salesNameSpinner=findViewById(R.id.salesNameSpinner);
-        excelConvert=findViewById(R.id.excelConvert);
-        pdfConvert=findViewById(R.id.pdfConvert);
-        share=findViewById(R.id.share);
+        fromDate = findViewById(R.id.from_date_r);
+        toDate = findViewById(R.id.to_date_r);
+        listCustomerLogReport = findViewById(R.id.listCustomerLogReport);
+        previewButton = findViewById(R.id.previewButton);
+        salesNameSpinner = findViewById(R.id.salesNameSpinner);
+        excelConvert = findViewById(R.id.excelConvert);
+        pdfConvert = findViewById(R.id.pdfConvert);
+        share = findViewById(R.id.share);
         backBtn = findViewById(R.id.backBtn);
-        globelFunction=new GlobelFunction(CustomerLogReport.this);
-        toDay=globelFunction.DateInToday();
+        globelFunction = new GlobelFunction(CustomerLogReport.this);
+        toDay = globelFunction.DateInToday();
         fromDate.setText(toDay);
         toDate.setText(toDay);
-        customerLogReportList=new ArrayList<>();
+        customerLogReportList = new ArrayList<>();
         fillSalesManSpinner();
-        importData=new ImportData(CustomerLogReport.this);
-            try {
-                String no= globelFunction.getsalesmanNum(salesNameSpinner.getSelectedItem().toString());
+        importData = new ImportData(CustomerLogReport.this);
+        try {
+            String no = globelFunction.getsalesmanNum(salesNameSpinner.getSelectedItem().toString());
 
-                settingModel=databaseHandler.getAllSetting();
-            if( settingModel.getImport_way().equals("0"))
-                importData.getCustomerLogReport(CustomerLogReport.this,"-1",toDay,toDay);
-            else if( settingModel.getImport_way().equals("1"))
-                importData.IIS_getCustomerLogReport(CustomerLogReport.this,no,toDay,toDay);
+            settingModel = databaseHandler.getAllSetting();
+            if (settingModel.getImport_way().equals("0"))
+                importData.getCustomerLogReport(CustomerLogReport.this, "-1", toDay, toDay);
+            else if (settingModel.getImport_way().equals("1"))
+                importData.IIS_getCustomerLogReport(CustomerLogReport.this, no, toDay, toDay);
 
-        }catch (Exception e){}
+        } catch (Exception e) {
+        }
 
-           previewButton.setOnClickListener(onClick);
+        previewButton.setOnClickListener(onClick);
         fromDate.setOnClickListener(onClick);
         toDate.setOnClickListener(onClick);
         excelConvert.setOnClickListener(onClick);
@@ -98,34 +118,70 @@ public class CustomerLogReport extends AppCompatActivity {
         share.setOnClickListener(onClick);
         backBtn.setOnClickListener(v -> onBackPressed());
 
+        bottom_navigation = findViewById(R.id.bottom_navigation);
+
+        bottom_navigation.setSelectedItemId(R.id.action_reports);
+
+        bottom_navigation.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+
+                            case R.id.action_plan:
+
+                                startActivity(new Intent(getApplicationContext(), PlanSalesMan.class));
+                                overridePendingTransition(0, 0);
+
+                                return true;
+
+                            case R.id.action_reports:
+
+                                return true;
+
+                            case R.id.action_location:
+
+                                return true;
+
+                            case R.id.action_notifications:
+
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                overridePendingTransition(0, 0);
+
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+
 
     }
 
-    View.OnClickListener onClick=new View.OnClickListener() {
+    View.OnClickListener onClick = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
 
 
-            switch (view.getId()){
+            switch (view.getId()) {
 
-                case R.id.previewButton :
+                case R.id.previewButton:
                     previewFunction();
                     break;
 
-                case R.id.from_date_r :
+                case R.id.from_date_r:
                     globelFunction.DateClick(fromDate);
                     break;
 
-                case R.id.to_date_r :
+                case R.id.to_date_r:
                     globelFunction.DateClick(toDate);
                     break;
-                case R.id.excelConvert :
+                case R.id.excelConvert:
                     convertToExcel();
                     break;
-                case R.id.pdfConvert :
+                case R.id.pdfConvert:
                     convertToPdf();
                     break;
-                case R.id.share :
+                case R.id.share:
                     shareWhatsApp();
                     break;
 
@@ -134,72 +190,72 @@ public class CustomerLogReport extends AppCompatActivity {
         }
     };
 
-    public void shareWhatsApp(){
+    public void shareWhatsApp() {
         try {
 
-            globelFunction.shareWhatsAppA(convertToPdf(),1);
+            globelFunction.shareWhatsAppA(convertToPdf(), 1);
         } catch (Exception e) {
             Toast.makeText(this, "Storage Permission", Toast.LENGTH_SHORT).show();
         }
     }
 
     private File convertToPdf() {
-        File file=null;
+        File file = null;
         try {
 
-            PdfConverter pdf =new PdfConverter(CustomerLogReport.this);
-             file=     pdf.exportListToPdf(customerLogReportList,"Customer Log Report",toDay,1);
-         } catch (Exception e) {
+            PdfConverter pdf = new PdfConverter(CustomerLogReport.this);
+            file = pdf.exportListToPdf(customerLogReportList, "Customer Log Report", toDay, 1);
+        } catch (Exception e) {
             Toast.makeText(this, "Storage Permission", Toast.LENGTH_SHORT).show();
         }
-   return file;
+        return file;
     }
 
     private void convertToExcel() {
-      try {
+        try {
             ExportToExcel exportToExcel = new ExportToExcel();
             exportToExcel.createExcelFile(CustomerLogReport.this, "CustomerLogReport.xls", 1, customerLogReportList);
-     } catch (Exception e) {
-     Log.e(" convertToExcel",e.getMessage());
-           Toast.makeText(this, "Storage Permission", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e(" convertToExcel", e.getMessage());
+            Toast.makeText(this, "Storage Permission", Toast.LENGTH_SHORT).show();
 
-  }
+        }
 
     }
 
 
-    public void previewFunction(){
+    public void previewFunction() {
 
-        String payKind="-1";
-        String salesNo="-1";
+        String payKind = "-1";
+        String salesNo = "-1";
 
 
+        int positionSales = salesNameSpinner.getSelectedItemPosition();
 
-        int positionSales=salesNameSpinner.getSelectedItemPosition();
-
-        if (positionSales == 0 || positionSales==-1) {
+        if (positionSales == 0 || positionSales == -1) {
             salesNo = "-1";
-            Log.e("salesNo-1", "" + salesNo +"  ");
+            Log.e("salesNo-1", "" + salesNo + "  ");
 
         } else {
             salesNo = salesManInfosList.get(positionSales - 1).getSalesManNo();
             Log.e("salesNo", "" + salesNo + "   name ===> " + salesManInfosList.get(positionSales - 1).getSalesName() + "    " + positionSales);
         }
-        String no= globelFunction.getsalesmanNum(salesNameSpinner.getSelectedItem().toString());
-        if( settingModel.getImport_way().equals("0"))
-        importData.getCustomerLogReport(CustomerLogReport.this,salesNo,fromDate.getText().toString(),toDate.getText().toString());
-        else   if( settingModel.getImport_way().equals("1"))
-            importData.IIS_getCustomerLogReport(CustomerLogReport.this,no,fromDate.getText().toString(),toDate.getText().toString());
+        String no = globelFunction.getsalesmanNum(salesNameSpinner.getSelectedItem().toString());
+        if (settingModel.getImport_way().equals("0"))
+            importData.getCustomerLogReport(CustomerLogReport.this, salesNo, fromDate.getText().toString(), toDate.getText().toString());
+        else if (settingModel.getImport_way().equals("1"))
+            importData.IIS_getCustomerLogReport(CustomerLogReport.this, no, fromDate.getText().toString(), toDate.getText().toString());
     }
 
-    public void fillSalesManSpinner(){
+    public void fillSalesManSpinner() {
 
         salesNameSpinnerAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, salesManNameList);
         salesNameSpinner.setAdapter(salesNameSpinnerAdapter);
 
     }
-    public void fillCustomerLogReport(){
-        customerLogReportAdapter=new CustomerLogReportAdapter(CustomerLogReport.this,customerLogReportList);
+
+    public void fillCustomerLogReport() {
+        customerLogReportAdapter = new CustomerLogReportAdapter(CustomerLogReport.this, customerLogReportList);
         listCustomerLogReport.setAdapter(customerLogReportAdapter);
 
     }
