7369f12142888d8db190ccff243c84052fd32cd0
abeer <sundos.tamomo20@gmail.com>
2023-05-27 13:36:20 +0300

    edit..

--- app/src/main/java/com/example/adminvansales/LogIn.java
+++ app/src/main/java/com/example/adminvansales/LogIn.java
@@ -92,7 +92,7 @@ public class LogIn extends AppCompatActivity {
     public  static  int hideRawahne=0;
 
 
-    
+
 
     public static Context contextG;
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
